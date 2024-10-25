614b28f70a8cee1c1643806d72b437df7f85e535
gunheekwon <61641049+gunheekwon@users.noreply.github.com>
2024-10-25 14:15:49 +0000

    20241025 많이 못함ㅠ

--- 13975.c
+++ 13975.c
@@ -1,27 +1,41 @@
 #include<stdio.h>
+// gg 몰?루
+// 처음부터 안 합쳐도 됨 <- 요거를 고려하지 않았음
+// 순서 상관 없이 합칠 수 있다는 것을 고려해서 다시 풀어보자!
+int tot_ans= 0;
 
 int abs(int x){
     return x>0?x:-x;
 }
 
 int f(int *a, int *sum, int s, int e){
-    printf("f(%d, %d)\n", s, e);
     if(s>=e) return a[s];
-    if(s+1==e) return a[s]+a[e];
+    if(s+1==e) {
+        return a[s]+a[e];
+    }
 
-    int ne, tmp, min, tot;
-    tot= sum[e]-sum[s-1];
+    int ne, tmp1, tmp2, tmp3, min;
     ne= s;
-    tmp= 0;
+    tmp1= 0;
+    tmp2= sum[e]-sum[s-1];
+    min= abs(tmp1-tmp2);
     for(int j= s; j<e; j++){
-        tmp+=a[j];
-        if(min>abs(tot-2*tmp)){
-            min= abs(tot-2*tmp);
+        tmp1+=a[j];
+        tmp2-=a[j];
+        tmp3= abs(tmp1-tmp2);
+        if(min>tmp3){
+            min= tmp3;
             ne= j;
         }
     }
-
-    return f(a, sum, s, ne) + f(a, sum, ne+1, e);
+    int ans1, ans2;
+    ans1= f(a, sum, s, ne);
+    ans2= f(a, sum, ne+1, e);
+    printf("f(%d, %d) \tans1: %d \t(%d, %d)\n", s, e, ans1, s, ne);
+    printf("f(%d, %d) \tans2: %d \t(%d, %d)\n", s, e, ans2, ne+1, e);
+    if(s!=ne) tot_ans+=ans1;
+    if(ne+1!=e) tot_ans+=ans2;
+    return ans1+ans2;
 }
 
 int main(){
@@ -39,6 +53,7 @@ int main(){
         }
         
         printf("%d\n", sum[k]+f(a, sum, 1, k));
+        printf("%d\n", tot_ans);
     }
 
 }
